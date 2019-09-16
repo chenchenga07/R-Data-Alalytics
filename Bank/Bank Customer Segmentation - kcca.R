86cc0ad021fda6560ee4acaa2098937e998d0f32
chenchenga07 <chenchenga077@gmail.com>
2019-09-15 21:06:54 -0400

    minor changes

--- Bank/Bank Customer Segmentation - kcca.R
+++ Bank/Bank Customer Segmentation - kcca.R
@@ -9,13 +9,10 @@ customerDataPrep = customerData
 customerDataPrep[,1] = as.factor(customerDataPrep[,1]) # zip_code
 customerDataPrep[,2] = as.factor(customerDataPrep[,2]) # country_code
 customerDataPrep[,12] = as.factor(customerDataPrep[,12]) # occupation
-
-# change categorical variables into numeric
-library(dummies)
-customerDataPrep = dummy.data.frame(customerDataPrep, names = c("gender","marital_status"))
+summary(customerDataPrep)
 
 # drop variables not used in the following segmentation modeling
-drop = c("zip_code","county_code","city","state","county_name","gender","marital_status","income","occupation",
+drop = c("zip_code","county_code","county_name","income","occupation",
          "child0_3","child4_6","child7_9","child10_11","child13_18","home_owner","dwelling_type",
          "great_outdoors","sporting_life","health_fitness","luxury_life","doit_yourselfer","truck_owner","motor_cycle","sports","entertainment_enth","hobbyists",
          "avid_readers","collectors","travel","pets","music","toys","art_craft","gardening","family","food","cars","casino_gambling","contributors",
@@ -29,8 +26,6 @@ customerDataPrep$year_structure =
   ifelse(is.na(customerDataPrep$year_structure), median(customerDataPrep$year_structure,na.rm = T), 
          customerDataPrep$year_structure)
 
-summary(customerDataPrep)
-
 # replace NA with 0 in some columns
 c = c("education_code","children","contributor_index")
 customerDataPrep[c][is.na(customerDataPrep[c])] = 0
@@ -40,34 +35,32 @@ summary(customerDataPrep)
 customerDataPrep = customerDataPrep[complete.cases(customerDataPrep),]
 
 
-# rescale data
+# rescale all numeric variables
 rescale = function(r) {
   if (sd(r) != 0) 
     res = (r - mean(r))/sd(r) else res = 0 * r
     return(res)
 }
 
-segmentDataScaled = apply(customerDataPrep,2,rescale)
-ncol(segmentDataScaled)
-# 15 variables
+numericVariablesScaled = apply(customerDataPrep[,5:15],2,rescale)
+customerDataPrep = cbind(customerDataPrep[,1:4],numericVariablesScaled)
+
 save.image(file="Bank.Rdata")
 
 
-### convert multiple variables into a smaller group of components/factors for the following modeling
+### convert multiple numeric variables into a smaller group of components/factors for the following modeling
 
 # approach 1: principal component analysis (PCA)
-pca.out = prcomp(segmentDataScaled,scale=T)
+pca.out = prcomp(numericVariablesScaled,scale=T)
 print(pca.out)
 summary(pca.out) 
 # check cucumlative proportion
-# first 6 components explain around 70% of variances
-# PC1 highly related with num_in_hhld, marital_statusM
-# PC2 highly related with genderM
-# PC3 highly related with num_of_children(-), children(-)
-# PC4 highly related with education_code, age
-# PC5 highly related with year_structure(-), median_home_value
-# PC6 highly related with median_income, age
-
+# first 5 components explain around 70% of variances
+# PC1 highly related with num_in_hhld(-), num_of_children(-)
+# PC2 highly related with age, len_of_residence
+# PC3 highly related with contributor_index(-), education_code(-)
+# PC4 highly related with year_structure, median_home_value(-)
+# PC5 highly related with median_income(-)
 
 plot(pca.out) 
 # check how many components' variances are above 1 / has big drop
@@ -80,16 +73,32 @@ plot(cumsum(pca.pve)) # visualize
 # approach 2: factor analysis
 library(GPArotation)
 library(psych)
-fa.parallel(segmentDataScaled,fa="fa",n.iter=30,main="Scree plots with parallel analysis")
+fa.parallel(numericVariablesScaled,fa="fa",n.iter=30,main="Scree plots with parallel analysis")
 # sharp breaks in the plot suggest the appropriate number of factors to extract
-f1 = fa(segmentDataScaled,rotate="Varimax",nfactors=2)
-plot(f1)
+# parallel analysis suggests that the number of factors =  5 
 
-fa1 = factanal(segmentDataScaled,factors = 2,rotation = "varimax", scores = "regression")
+fa1 = factanal(numericVariablesScaled,factors = 5,rotation = "varimax", scores = "regression")
 fa1
 head(fa1$scores)
-# factor1 highly related with num_in_hhld, num_of_adults, num_of_children
-# factor2 highly related with genderM, genderF(-)
+# factor1 highly related with num_of_children, children, num_in_hhld
+# factor2 highly related with num_of_adults, num_in_hhld
+# factor3 highly related with age, education_code
+# factor4 highly related with contributor_index
+# factor5 highly related with median_home_value
+
+
+# combine string variables with first 5 components together for kcca clustering
+customerDataPrep = cbind(customerDataPrep[,1:4],pca.out$x[,1:5])
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -98,28 +107,28 @@ head(fa1$scores)
 # define how many clusters should be used
 
 # approach 1: pamk()
-pm = pamk(segmentDataScaled,scaling=T, usepam = F, criterion="multiasw")
+pm = pamk(numericVariablesScaled,scaling=T, usepam = F, criterion="multiasw")
 pm$nc
-# 2
+# 2 clusters
 
 # approach 2: wss plot 
 library(cluster) 
 library(fpc)
-set.seed(123)
+# set.seed(123)
 
-wss = (nrow(segmentDataScaled)-1)*sum(apply(segmentDataScaled,2,var))
-for (i in 1:ncol(segmentDataScaled)) wss[i] = sum(kmeans(segmentDataScaled,centers=i)$withinss)
-plot(1:ncol(segmentDataScaled), wss, type="b", xlab="Number of Clusters",ylab="Within groups sum of squares")
+wss = (nrow(numericVariablesScaled)-1)*sum(apply(numericVariablesScaled,2,var))
+for (i in 1:ncol(numericVariablesScaled)) wss[i] = sum(kmeans(numericVariablesScaled,centers=i)$withinss)
+plot(1:ncol(numericVariablesScaled), wss, type="b", xlab="Number of Clusters",ylab="Within groups sum of squares")
 #found 2,3,4 clusters maybe suitable in segmentation
 
 
-# use first 6 principal components to do the clustering
+# use first 5 principal components to do the clustering
 
 #plot
-km1 = kmeans(pca.out$x[,1:6],2,iter.max = 20, nstart=2)
+km1 = kmeans(pca.out$x[,1:5],2,iter.max = 20, nstart=2)
 # first 6 components, 2 clusters
-km2 = kmeans(pca.out$x[,1:6],3,iter.max = 20, nstart=2)
-km3 = kmeans(pca.out$x[,1:6],4,iter.max = 20, nstart=2)
+km2 = kmeans(pca.out$x[,1:5],3,iter.max = 20, nstart=2)
+km3 = kmeans(pca.out$x[,1:5],4,iter.max = 20, nstart=2)
 
 clusplot(pca.out$x[,1:6], km1$cluster, color=TRUE, shade=TRUE, labels=2, lines=0)
 plotcluster(pca.out$x[,1:6], km1$cluster)
@@ -128,14 +137,14 @@ plotcluster(pca.out$x[,1:6], km2$cluster)# the clearest one
 clusplot(pca.out$x[,1:6], km3$cluster, color=TRUE, shade=TRUE, labels=2, lines=0)
 plotcluster(pca.out$x[,1:6], km3$cluster)
 
-percsize = paste(1:3," = ",format(km2$size/sum(km2$size)*100,digits=2),"%",sep="")
-pie(km2$size,labels=percsize)
+percsize = paste(1:3," = ",format(km1$size/sum(km1$size)*100,digits=2),"%",sep="")
+pie(km1$size,labels=percsize)
 # segment 1 consists 38% of total records
 # segment 2 consists 24% of total records
 # segment 3 consists 38% of total records
 
-km2$centers
-scaledCenters = km2$centers
+km1$centers
+scaledCenters = km1$centers
 
 # scale back cluster centers
 options(scipen = 999)
@@ -158,8 +167,8 @@ segmentation3 = subset(customerData,customerData$clusterNo == 3)
 
 # check customers' interest (percentage)
 a = c("great_outdoors","sporting_life","health_fitness","luxury_life","doit_yourselfer","truck_owner","motor_cycle","sports","entertainment_enth","hobbyists",
-    "avid_readers","collectors","travel","pets","music","toys","art_craft","gardening","family","food","cars","casino_gambling","contributors",
-    "travel_business","travel_personal","travel_vacation","donor_arts_or_cultural")
+      "avid_readers","collectors","travel","pets","music","toys","art_craft","gardening","family","food","cars","casino_gambling","contributors",
+      "travel_business","travel_personal","travel_vacation","donor_arts_or_cultural")
 segment1 = rep(NA,27)
 segment2 = rep(NA,27)
 segment3 = rep(NA,27)
--- Bank/kcca sample.R
+++ Bank/kcca sample.R
@@ -1,11 +1,19 @@
 library(flexclust)
 
+# create sample data as matrix
+# cause one of the columns are string, all numbers are stored as strings
+# the result is a string matrix
 city = c("NYC","NYC","Los Angeles","Jersey City")
 income = c(20,16,10,4)
 rating = c(6,2,3,2)
 test = cbind(city,income,rating)
 str(test)
 
+# define distance function which calculate distance between 2 observations
+
+# if "city"(column 1) is different, return 1, else return 0
+# other columns' distance is calculated by absloute difference of 2 numbers
+# the total distance is sum of all 3 columns' distances
 
 distance = function(x,y) {
   if (x[1] == y[1])
@@ -25,19 +33,29 @@ distance(test[1,], test[2,])
 distance(test[1,], test[3,])
 
 
-distanceMatrix = function(df1,df2) {
-  m = matrix(rep(NA,nrow(df1)*nrow(df2)), nrow=nrow(df1), ncol=nrow(df2))
-  for (i in 1:nrow(df1)){
-    for (j in 1:nrow(df2)){
-      m[i,j] = distance(df1[i,],df2[j,])
+# create pair-wise distance matrix
+distanceMatrix = function(m1,m2) {
+  m = matrix(
+    rep(NA,nrow(m1)*nrow(m2)), 
+    nrow=nrow(m1), 
+    ncol=nrow(m2)
+  )
+  
+  for (i in 1:nrow(m1)){
+    for (j in 1:nrow(m2)){
+      m[i,j] = distance(m1[i,],m2[j,])
     }
   }
+  
   return(m)
 }
   
 distanceMatrix(test, test)
 
 
+# define cluster center
+# pick the most frequently showed city as center
+# calculate mean of income and rating as center
 centroid = function(df) {
   city = tail(names(sort(table(df[,1]))), 1)
   income = mean(as.numeric(df[,2]))
@@ -49,10 +67,16 @@ centroid = function(df) {
 centroid(test)
 
 
-
+# define kccaFamily with previous self-built functions
 cheng = kccaFamily(which=NULL, dist=distanceMatrix, cent=centroid, name="cheng",
            preproc = NULL, trim=0, groupFun = "minSumClusters")
+
+# run k-means clustering using the customized cheng function
 k = kcca(test, 2, family=cheng, weights=NULL, group=NULL,
      control=NULL, simple=FALSE, save.data=FALSE)
+
+# check how many clusters
 summary(k)
+
+# check cluster assignment of each observations
 clusters(k)
